b6d58b353a8d1bde79b8a3d28f039dd39b4d8b22
point1reiya <point_yukkuri@outlook.jp>
2018-12-01 23:32:28 +0900

    The First Commit

---
+++ index.js
@@ -0,0 +1,96 @@
+
+import { Synthesizer } from "./synthesizer.js";
+import { Recognizer } from "./recognizer.js";
+/*
+const urlAnswerer = "http://localhost:8000/answer?text=";
+const urlSynthesizer = "http://localhost:8000/synthesize?text=";
+const answerer = new Answerer({ url: urlAnswerer });
+const synthesizer = new Synthesizer({ url: urlSynthesizer });
+*/
+let flushFlag = false;
+let flushTimer = null;
+let clearTimer = null;
+
+function setInputText(text) {
+  document.getElementById("input_text").innerHTML = text;
+}
+
+function setOutputText(text) {
+  document.getElementById("output_text").innerHTML = text;
+}
+
+function startFlushTimer() {
+  flushFlag = false;
+  stopFlushTimer();
+  flushTimer = setTimeout(() => {
+    console.log("on flush");
+    flushFlag = true;
+    flushTimer = null;
+  }, 1000);
+}
+
+function stopFlushTimer() {
+  if (flushTimer != null) {
+    clearTimeout(flushTimer);
+    flushTimer = null;
+  }
+}
+
+function startClearTimer() {
+  stopClearTimer();
+  clearTimer = setTimeout(() => {
+    console.log("on clear");
+    setInputText("");
+    clearTimer = null;
+  }, 2500);
+}
+
+function stopClearTimer() {
+  if (clearTimer != null) {
+    clearTimeout(clearTimer);
+    clearTimer = null;
+  }
+}
+
+function onSpeechStart() {
+  console.log("on speech start");
+  startFlushTimer();
+}
+
+function onSpeechProcess(text, isPhraseStart, isPhraseEnd) {
+  console.log(
+    "on speech process",
+    "istart",
+    isPhraseStart,
+    "isend",
+    isPhraseEnd
+  );
+  if (isPhraseStart) {
+    startFlushTimer();
+  }
+
+  if (isPhraseEnd || flushFlag) {
+    setInputText(text);
+    startFlushTimer();
+  }
+
+  stopClearTimer();
+}
+
+function onSpeechStop(text) {
+  console.log("on speech stop");
+  setInputText(text);
+  // startClearTimer();
+  stopFlushTimer();
+
+
+}
+
+
+const recognizer = new Recognizer({
+  onSpeechStart,
+  onSpeechProcess,
+  onSpeechStop
+});
+
+window.onload = () => recognizer.recognize();
---
+++ tra.php
@@ -0,0 +1,4 @@
+<?php
+$json = file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?lang=ja-en&text='.$_GET["text"].'&format=html&key=trnsl.1.1.20181201T132435Z.c34b08a508601c8a.897ebb694a006ca2a895dadac1bb3bc2a5039117');
+$text = json_decode($json,true);
+echo $text["text"]["0"];
